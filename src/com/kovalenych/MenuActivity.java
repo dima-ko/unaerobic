a77b70aa1a918d136541a3ac22c4ff7ccd694cf8
Justin Tim <insomniac.robot@gmail.com>
2012-08-06 19:40:41 +0200

    start refactor2

--- src/com/kovalenych/MenuActivity.java
+++ src/com/kovalenych/MenuActivity.java
@@ -1,133 +1,15 @@
 package com.kovalenych;
 
 import android.app.Activity;
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.widget.*;
-import com.kovalenych.ranking.RankingActivity;
-import com.kovalenych.tables.TablesActivity;
 
 
-public class MenuActivity extends Activity {
-
-//    GoogleAnalyticsTracker tracker;
-
-
-    /**
-     * Called when the activity is first created.
-     */
-
+public class MenuActivity extends Activity
+{
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        Utils.resolvePlatform(this);
-        int buttonDim = Utils.dpToPix(52);
-        int buttonMarg = Utils.dpToPix(12);
-        int smDim = (Utils.width < Utils.height) ? Utils.width : Utils.height;
-        setContentView(R.layout.main);
-        RelativeLayout parent = (RelativeLayout) findViewById(R.id.parent);
-
-        Button button1 = new Button(this);
-        button1.setBackgroundResource(R.drawable.qeust);
-        RelativeLayout.LayoutParams params1 = new RelativeLayout.LayoutParams(buttonDim, buttonDim);
-        params1.setMargins(buttonMarg, buttonMarg, smDim - buttonDim - buttonMarg, buttonMarg);
-        params1.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
-        params1.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
-        parent.addView(button1, params1);
-
-        Button button2 = new Button(this);
-        button2.setBackgroundResource(R.drawable.taable);
-        RelativeLayout.LayoutParams params2 = new RelativeLayout.LayoutParams(buttonDim, buttonDim);
-        params2.setMargins(buttonMarg, buttonMarg, smDim / 2 - buttonDim / 2, buttonMarg );
-        params2.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
-        params2.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
-        parent.addView(button2, params2);
-
-        Button button3 = new Button(this);
-        button3.setBackgroundResource(R.drawable.vidos);
-        RelativeLayout.LayoutParams params3 = new RelativeLayout.LayoutParams(buttonDim, buttonDim);
-        params3.setMargins(buttonMarg, buttonMarg, buttonMarg, buttonMarg);
-        params3.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
-        params3.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
-        parent.addView(button3, params3);
-
-        Button button4 = new Button(this);
-        button4.setBackgroundResource(R.drawable.book2);
-        RelativeLayout.LayoutParams params4 = new RelativeLayout.LayoutParams(buttonDim, buttonDim);
-        params4.setMargins(buttonMarg, buttonMarg, buttonMarg, smDim - buttonDim - buttonMarg);
-        params4.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
-        params4.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
-        parent.addView(button4, params4);
-
-        Button button5 = new Button(this);
-        button5.setBackgroundResource(R.drawable.infoblack);
-        RelativeLayout.LayoutParams params5 = new RelativeLayout.LayoutParams(buttonDim, buttonDim);
-        params5.setMargins(buttonMarg, buttonMarg, buttonMarg , smDim / 2 - buttonDim / 2);
-        params5.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
-        params5.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
-        parent.addView(button5, params5);
-
-        Button button6 = new Button(this);
-        button6.setBackgroundResource(R.drawable.heart_beat);
-        RelativeLayout.LayoutParams params6 = new RelativeLayout.LayoutParams(buttonDim, buttonDim);
-
-        int rad = (smDim - buttonDim) /2;
-        params6.setMargins(buttonMarg, buttonMarg, rad, rad);
-        params6.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
-        params6.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
-        parent.addView(button6, params6);
-
-    }
-
-//    private void startActivities() {
-//        int act = 0;
-//        Intent intent;
-//        switch (act) {
-//            case 0:
-////                intent = new Intent(MenuActivity.this, InfoActivity.class);
-//                break;
-//            case 1:
-//                Toast.makeText(MenuActivity.this, MenuActivity.this.getString(R.string.under_construction), Toast.LENGTH_SHORT).show();
-//                break;
-//            case 2:
-//                intent = new Intent(MenuActivity.this, TablesActivity.class);
-//                break;
-//            case 3:
-//                if (haveInternet())
-//                    intent = new Intent(MenuActivity.this, NostraVideoActivity.class);
-//                else
-//                    Toast.makeText(MenuActivity.this, MenuActivity.this.getString(R.string.noConnect), Toast.LENGTH_SHORT).show();
-//                break;
-//            case 4:
-////                if (haveInternet())
-//                intent = new Intent(MenuActivity.this, RankingActivity.class);
-////                else
-////                    Toast.makeText(MenuActivity.this, MenuActivity.this.getString(R.string.noConnect), Toast.LENGTH_SHORT).show();
-//                break;
-//            case 5:
-//                if (haveInternet())
-//                    intent = new Intent(MenuActivity.this, ArticlesActivity.class);
-//                else
-//                    Toast.makeText(MenuActivity.this, MenuActivity.this.getString(R.string.noConnect), Toast.LENGTH_SHORT).show();
-//                break;
-//
-//        }
-//    }
-
-    private boolean haveInternet() {
-        NetworkInfo info = ((ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE)).getActiveNetworkInfo();
-        if (info == null || !info.isConnected()) {
-            return false;
-        }
-        if (info.isRoaming()) {
-            // here is the roaming option you can change it if you want to disable internet while roaming, just return false
-            return true;
-        }
-        return true;
+    public void onCreate(Bundle savedInstanceState)
+    {
     }
 
 }
--- src/com/kovalenych/PlatformResolver.java
+++ src/com/kovalenych/PlatformResolver.java
@@ -9,10 +9,8 @@ public class PlatformResolver {
     private Context _context;
     public static boolean isHVGA = false;
     public static boolean isQVGA = false;
-    public static boolean isDONUT = false;
     public static boolean is720p = false;
     public static boolean isTabI = false;
-    public static boolean is854 = false;
 
 
     public PlatformResolver(Context context) {
@@ -37,187 +35,35 @@ public class PlatformResolver {
     }
 
     static public int getRank() {
-        if (isHVGA)
-            return R.layout.ranking_hvga;
-        else if (is720p)
-            return R.layout.ranking720p;
-        else if (isTabI)
-            return R.layout.ranking;
-        else
             return R.layout.ranking;
     }
 
     static public int getTableLayout() {
-        if (isHVGA)
-            return R.layout.table_hvga;
-        else
             return R.layout.table;
     }
 
     static public int getTableItemLayout() {
-        if (isHVGA)
-            return R.layout.table_item_hvga;
-        else
             return R.layout.table_item;
     }
 
     static public int getClocksLayout() {
-        if (isHVGA)
-            return R.layout.clocks_hvga;
-        else
             return R.layout.clocks;
     }
 
     static public int getMenuLayout() {
-        if (isHVGA)
-            return R.layout.tables_hvga;
-        else
             return R.layout.tables;
     }
 
     static public int getNewCycleDialogLayout() {
-        if (isHVGA)
-            return R.layout.new_cycle_dialog_hvga;
-        else
             return R.layout.new_cycle_dialog;
     }
 
     static public int getVoicesLayout() {
-        if (isHVGA)
-            return R.layout.voice_hvga;
-        else
             return R.layout.voice;
     }
 
-    static public int getMenuBG() {
-        if (is720p)
-            return R.drawable.bg720p;
-        if (isQVGA)
-            return R.drawable.bg_qwga;
-        if (isHVGA)
-            return R.drawable.bg_hwga;
-        else
-            return R.drawable.bg;
-    }
-
-    static public int getRadius() {
-        if (is720p)
-            return 460;
-        if (isQVGA)
-            return 130;
-        if (isHVGA)
-            return 200;
-        if (isTabI)
-            return 375;
-        else
-            return 300;
-    }
-
-    static public int getCenterX() {
-        if (isQVGA)
-            return 210;
-        if (isHVGA)
-            return 280;
-        if (is720p)
-            return 700;
-        if (isTabI)
-            return 570;
-        else
-            return 425;
-    }
-
-    static public int getCenterY() {
-        if (isQVGA)
-            return 310;
-        if (isHVGA)
-            return 465;
-        if (is720p)
-            return 1200;
-        if (isTabI)
-            return 980;
-        if (is854)
-            return 799;
-        else
-            return 745;
-    }
-
-    static public int getCircleShiftX() {
-        if (isQVGA)
-            return -15;
-        if (isHVGA)
-            return -35;
-        else
-            return -35;
-    }
-
-    static public int getCircleShiftY() {
-        if (isQVGA)
-            return -43;
-        if (isHVGA)
-            return -55;
-        else
-            return -55;
-    }
-
-
     static public int getRecordItemLayout() {
-        if (isHVGA)
-            return R.layout.record_item_hwga;
-        else
             return R.layout.record_item;
     }
 
-
-    static public int getCycleItemLayout() {
-        if (isHVGA)
-            return R.layout.cycle_item;
-        else
-            return R.layout.cycle_item;
-    }
-
-
-    public static int[] getLabelsX() {
-        if (isTabI)
-            return new int[]{315, 470, 35, 210, 315, 207};
-        if (is720p)
-            return new int[]{380, 580, 45, 260, 370, 230};
-        if (isQVGA)
-            return new int[]{95, 155, -15, 48, 85, 60};
-        if (isHVGA)
-            return new int[]{133, 225, -10, 80, 140, 75};
-        else
-            return new int[]{215, 360, 30, 160, 245, 142};
-
-    }
-
-    public static int[] getLabelsY() {
-        if (isTabI)
-            return new int[]{780, 690, 405, 305, 160, 945};
-        if (is720p)
-            return new int[]{950, 830, 500, 380, 180, 1150};
-        if (isQVGA)
-            return new int[]{255, 221, 123, 91, 38, 304};
-        if (isHVGA)
-            return new int[]{371, 320, 190, 145, 65, 460};
-        if (is854)
-            return new int[]{605 + 54, 540 + 54, 325, 255, 125, 735 + 54};
-        else
-            return new int[]{605, 540, 325, 255, 125, 735};
-
-    }
-
-
-    public static float getTextSize() {
-        if (is720p)
-            return 23;
-        else
-            return 16;
-    }
-
-    public static int getTextWidth() {
-        if (is720p)
-            return 130;
-        else
-            return 100;
-    }
 }
