e4d720daec0044ce0a065b20a3f4fe151cd250b1
Justin Tim <insomniac.robot@gmail.com>
2012-09-02 13:20:54 +0200

    notif fixed 6

---
+++ src/com/kovalenych/media/SearchResponse.java
@@ -0,0 +1,19 @@
+package com.kovalenych.media;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class SearchResponse {
+
+    public List<Video> videos;
+
+    @SerializedName("max_id")
+    public long maxId;
+
+    @SerializedName("since_id")
+    public int sinceId;
+
+    public String query;
+
+}
