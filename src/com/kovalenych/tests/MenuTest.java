bb330feae2189aa5668bdbbc51b82b08c45cced6
Justin Tim <insomniac.robot@gmail.com>
2012-10-06 20:40:18 +0200

    added Tests

--- src/com/kovalenych/tests/MenuTest.java
+++ src/com/kovalenych/tests/MenuTest.java
@@ -1,12 +1,9 @@
-package com.kovalenych.test;
+package com.kovalenych.tests;
 
 import android.test.ActivityInstrumentationTestCase2;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import com.jayway.android.robotium.solo.Solo;
 import com.kovalenych.MenuActivity;
-import junit.framework.Assert;
 
 import java.util.List;
 
@@ -25,55 +22,31 @@ public class MenuTest extends
     public MenuTest() {
         super("com.kovalenych",
                 MenuActivity.class);
+
     }
 
     public void setUp() throws Exception {
-//        super.setUp();
-//        this.activity = this.getActivity();
         solo = new Solo(getInstrumentation(), getActivity());
     }
-
-//     solo.sendKey(Solo.MENU);
-//    solo.clickOnText("More");
-//    solo.clickOnText("Preferences");
-//    solo.clickOnText("Edit File Extensions");
-//    Assert.assertTrue(solo.searchText("rtf"));
-//
-//    solo.clickOnText("txt");
-//    solo.clearEditText(2);
-//    solo.enterText(2, "robotium");
-//    solo.clickOnButton("Save");
-//    solo.goBack();
-//    solo.clickOnText("Edit File Extensions");
-//    Assert.assertTrue(solo.searchText("application/robotium"));
 
     public void testSettings() throws Exception {
 
 
-        solo.waitForFragmentById(1);
+//        solo.clickOnText("Таблицы");
+//        solo.sleep(1000);
+//        solo.clickOnText("Рекорды");
+//        solo.sleep(1000);
+//        solo.clickOnText("Статьи");
+//        solo.sleep(1000);
 ////        solo.clickOnText();
 //        clickOnButtonByID(R.id.settings);
 //        solo.sleep(1000);
 //        View settingsDialog = solo.getView(SmsActivity.SETDIALOG_ID);
 //        Assert.assertTrue(settingsDialog.getVisibility() == View.VISIBLE);
-//
-//        clickOnButtonByID(R.id.settings);
-//        solo.sleep(2000);
-//        Assert.assertFalse(settingsDialog.getVisibility() != View.VISIBLE);
 
     }
 
-    public void testLife() throws Exception {
-//        EditText numberEdit = (EditText) solo.getView(R.id.number_edit);
-//        solo.clickOnView(numberEdit);
-//        solo.enterText(numberEdit, "0937062222");
-//
-//        EditText textEdit = (EditText) solo.getView(R.id.text_edit);
-//        solo.clickOnView(textEdit);
-//        solo.enterText(textEdit, "asdasdaasdasdasdasdasdasdasdasdas");
-//        clickOnButtonByID(R.id.sendbutton);
-//        solo.sleep(1000);
-//        solo.assertCurrentActivity("Expected MainActivity to launch", SmsActivity.class);
+    public void testLifeCycle() throws Exception {
 
     }
 
