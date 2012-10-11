044b26a80a8c663f866388b3c02cf7fa2cfe383c
Justin Tim <insomniac.robot@gmail.com>
2012-10-11 20:26:29 +0200

    flags added

--- src/com/kovalenych/roboto/EditTextRoboto.java
+++ src/com/kovalenych/roboto/EditTextRoboto.java
@@ -3,6 +3,7 @@ package com.kovalenych.roboto;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.EditText;
+import com.kovalenych.R;
 import com.kovalenych.Utils;
 
 /**
@@ -10,19 +11,28 @@ import com.kovalenych.Utils;
  * by insomniac and angryded
  * for their purposes
  */
-public class EditTextRoboto  extends EditText{
+public class EditTextRoboto extends EditText {
     public EditTextRoboto(Context context) {
         super(context);
         setTypeface(Utils.roboto_light);
+        setBackgroundResource(R.drawable.edit_bl);
+        setTextColor(0xffffffff);
+        setHintTextColor(0xff333333);
     }
 
     public EditTextRoboto(Context context, AttributeSet attrs) {
         super(context, attrs);
         setTypeface(Utils.roboto_light);
+        setBackgroundResource(R.drawable.edit_bl);
+        setTextColor(0xffffffff);
+        setHintTextColor(0xff333333);
     }
 
     public EditTextRoboto(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         setTypeface(Utils.roboto_light);
+        setBackgroundResource(R.drawable.edit_bl);
+        setTextColor(0xffffffff);
+        setHintTextColor(0xff333333);
     }
 }
