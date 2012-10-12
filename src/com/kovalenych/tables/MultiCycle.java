24fca5da4b3944f5bb2be5c7c5f914c48b3960c6
Justin Tim <insomniac.robot@gmail.com>
2012-10-12 06:46:14 +0200

    flags added

---
+++ src/com/kovalenych/tables/MultiCycle.java
@@ -0,0 +1,12 @@
+package com.kovalenych.tables;
+
+import java.util.ArrayList;
+
+/**
+ * this class was made
+ * by insomniac and angryded
+ * for their purposes
+ */
+public class MultiCycle {
+    public ArrayList<Cycle> cycles;
+}
