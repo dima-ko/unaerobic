7431be34aebd0bb5cfc07dad4335392463a48191
Dmytro Kovalenko <kovalenych@gmail.com>
2012-10-22 09:35:14 +0200

    fix

--- src/com/kovalenych/UnaeroApplication.java
+++ src/com/kovalenych/UnaeroApplication.java
@@ -96,8 +96,9 @@ public class UnaeroApplication extends Application {
         db.close();
         dbHelper.close();
 
-        preferences.edit().putLong("lastUpd", new Date().getTime());
-        preferences.edit().commit();
+        SharedPreferences.Editor edit = preferences.edit();
+        edit.putLong("lastUpd", new Date().getTime());
+        edit.commit();
         //todo: notifyDataSetChanged();
     }
 
@@ -128,8 +129,9 @@ public class UnaeroApplication extends Application {
         dbHelper.close();
 
 
-        preferences.edit().putLong("lastUpd", new Date().getTime());
-        preferences.edit().commit();
+        SharedPreferences.Editor edit = preferences.edit();
+        edit.putLong("lastUpd", new Date().getTime());
+        edit.commit();
 
     }
 
